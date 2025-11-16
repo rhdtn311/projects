4aca9e24d32dd0f2aea1f513995834bda91f8377
kong <rhdtn311@github.com>
2025-11-16 21:13:04 +0900

    feat: 입력값 유효성 검증 코드 추가

--- pg-routing/src/main/kotlin/org/kong/pgrouting/presentation/dto/PaymentRequest.kt
+++ pg-routing/src/main/kotlin/org/kong/pgrouting/presentation/dto/PaymentRequest.kt
@@ -1,7 +1,13 @@
 package org.kong.pgrouting.presentation.dto
 
+import jakarta.validation.constraints.Size
+import org.hibernate.validator.constraints.Length
+
 data class PaymentRequest(
+    @field:Size(min = 1, max = 100)
     val amount: Long,
+    @field:Length(min = 1, max = 255)
     val productName: String,
+    @field:Length(min = 1, max = 50)
     val paymentMethod: String,
 )
